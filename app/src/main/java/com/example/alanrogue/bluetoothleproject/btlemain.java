49bc7ce33eb10443e747bdbf92b6d00b5a0dd8cb
alanrogue <alan7316@gmail.com>
2015-07-21 23:34:54 +0800

    Add HRM_Setup button function

--- app/src/main/java/com/example/alanrogue/bluetoothleproject/btlemain.java
+++ app/src/main/java/com/example/alanrogue/bluetoothleproject/btlemain.java
@@ -1,18 +1,36 @@
 package com.example.alanrogue.bluetoothleproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class btlemain extends Activity {
+
+    private Button mBtnHRMSetup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_btlemain);
+
+        mBtnHRMSetup = (Button)findViewById(R.id.btnSetup);
+
+        mBtnHRMSetup.setOnClickListener(btnHRMSetupOnClick);
     }
+
+    private View.OnClickListener btnHRMSetupOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent hrmSetup_it = new Intent();
+            hrmSetup_it.setClass(btlemain.this, hrm_setup.class);
+            startActivity(hrmSetup_it);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
